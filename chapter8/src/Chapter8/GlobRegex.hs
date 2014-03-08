88962fa0eb86e70cb179443842dd53c240d61b51
cbpark <cbpark@gmail.com>
2014-03-08 04:20:35 +0100

    Update chapter8

---
+++ chapter8/src/Chapter8/GlobRegex.hs
@@ -0,0 +1,37 @@
+module Chapter8.GlobRegex
+    ( globToRegex
+    , matchesGlob
+    ) where
+
+import           Text.Regex.Posix ((=~))
+
+globToRegex :: String -> String
+globToRegex cs = '^' : globToRegex' cs ++ "$"
+
+globToRegex' :: String -> String
+globToRegex' "" = ""
+
+globToRegex' ('*':cs) = ".*" ++ globToRegex' cs
+
+globToRegex' ('?':cs) = '.' : globToRegex' cs
+
+globToRegex' ('[':'!':c:cs) = "[^" ++ c : charClass cs
+
+globToRegex' ('[':c:cs) = '[' : c : charClass cs
+
+globToRegex' ('[':_) = error "undetermined character class"
+
+globToRegex' (c:cs) = escape c ++ globToRegex' cs
+
+escape :: Char -> String
+escape c | c `elem` regexChars = '\\' : [c]
+         | otherwise           = [c]
+         where regexChars = "\\+()^$.{}]|"
+
+charClass :: String -> String
+charClass (']':cs) = ']' : globToRegex' cs
+charClass (c:cs)   = c : charClass cs
+charClass []       = error "undetermined character class"
+
+matchesGlob :: FilePath -> String -> Bool
+name `matchesGlob` pat = name =~ globToRegex pat
