11d37f44fca751019615d41c3e70101cde9670f5
cbpark <cbpark@gmail.com>
2014-03-13 00:44:41 +0100

    Update chapter15

---
+++ chapter15/src/Chapter15/RandomSupply.hs
@@ -0,0 +1,10 @@
+module Chapter15.RandomSupply where
+
+import           Chapter15.Supply
+
+import           System.Random    hiding (next)
+
+randomsIO :: Random a => IO [a]
+randomsIO = getStdRandom $ \g ->
+            let (a, b) = split g
+            in (randoms a, b)
