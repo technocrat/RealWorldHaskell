f96edd120293b7f14eb9d2af29f1c4aa3134da0e
cbpark <cbpark@gmail.com>
2015-01-30 02:36:05 +0900

    Update chapter18

--- chapter18/src/Chapter18/CountEntries.hs
+++ chapter18/src/Chapter18/CountEntries.hs
@@ -1,18 +1,21 @@
 module Chapter18.CountEntries where
 
-import           Control.Monad    (forM, liftM)
-import           System.Directory (doesDirectoryExist, getDirectoryContents)
-import           System.FilePath  ((</>))
+import           Control.Applicative ((<$>), (<*>))
+import           Control.Monad       (forM, liftM)
+import           System.Directory    (doesDirectoryExist, getDirectoryContents)
+import           System.FilePath     ((</>))
 
 listDirectory :: FilePath -> IO [String]
 listDirectory = liftM (filter notDots) . getDirectoryContents
-    where notDots p = p /= "." && p /= ".."
+  -- where notDots p = p /= "." && p /= ".."
+  where notDots = (&&) <$> (/=".") <*> (/="..")
 
 countEntriesTrad :: FilePath -> IO [(FilePath, Int)]
 countEntriesTrad path = do
   contents <- listDirectory path
-  rest <- forM contents $ \name -> do
-                          let newName = path </> name
-                          isDir <- doesDirectoryExist newName
-                          if isDir then countEntriesTrad newName else return []
+  rest <- forM contents $ \name -> do let newName = path </> name
+                                      isDir <- doesDirectoryExist newName
+                                      if isDir
+                                        then countEntriesTrad newName
+                                        else return []
   return $ (path, length contents) : concat rest
